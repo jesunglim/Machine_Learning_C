c3ce53ea9db9257f9a2c55d3381599e677161514
Jay Lim <59774709+jaylimcode@users.noreply.github.com>
2021-05-03 00:35:29 +0900

    Update linear_regression.c

--- linear_regression.c
+++ linear_regression.c
@@ -1,12 +1,28 @@
 #include<stdio.h>
-#define M 4 // x,y array size
+#include<stdlib.h>
+#include<time.h>
+#define M 6 // x,y array size
 
 int main()
 {
 	// X and Y data
-	double x_train[] = { 2, 4, 6, 8 };
-	double y_train[] = { 81, 93, 91, 97 };
+	double x_train[M];
+	double y_train[M];
+	srand(time(NULL));
+	for (int i = 0; i < M; i++)
+	{
+		int r = (rand() % 100) + 1;
+		y_train[i] = r;
+	}
+	for (int i = 0; i < M; i++)
+		x_train[i] = 0.2 * y_train[i] * 0.5;
+	//
+	for (int i = 0; i < M; i++)
+		printf("x_train[%d] = %lf\n", i, x_train[i]);
+	for (int i = 0; i < M; i++)
+		printf("y_train[%d] = %lf\n", i, y_train[i]);
 
+	//
 	double W = 0.5; // Weight
 	double b = 0.5; // bias
 
@@ -18,7 +34,7 @@ int main()
 
 	// Minimize
 	double learning_rate = 0.01;
-	
+
 
 	for (int step = 0; step < 3000; step++)
 	{
@@ -32,39 +48,41 @@ int main()
 
 		// Cost Function
 		double tempCost = 0;
-		for (int i = 0; i < M; i++) 
+		for (int i = 0; i < M; i++)
 		{
 			costIndex[i] = hypothesis[i] - y_train[i];
-			costIndex[i] = costIndex[i] * costIndex[i]; // square
+			costIndex[i] *= costIndex[i]; // square
 		}
 		for (int i = 0; i < M; i++)
 			tempCost = tempCost + costIndex[i];
 		tempCost = tempCost / M;
 		cost = tempCost;
 
-		// W = learning_rate * ((hypothesis-y)*x).mean
+		// W = W - learning_rate * ((hypothesis-y)*x).mean
 		for (int i = 0; i < M; i++)
 			temp0Index[i] = hypothesis[i] - y_train[i];
 		for (int i = 0; i < M; i++)
-			temp0Index[i] = temp0Index[i] * x_train[i];
+			temp0Index[i] *= x_train[i]; // square
 		for (int i = 0; i < M; i++)
-			temp0 = temp0 + temp0Index[i];
+			temp0 += temp0Index[i];
 		temp0 = W - (learning_rate * (temp0 / M));
 
-		// b = learning_rate * (hypothesis-y).mean
+		// b = b - learning_rate * (hypothesis-y).mean
 		for (int i = 0; i < M; i++)
 			temp1Index[i] = hypothesis[i] - y_train[i];
 		for (int i = 0; i < M; i++)
-			temp1 = temp1 + temp1Index[i];
+			temp1 += temp1Index[i];
 		temp1 = b - (learning_rate * (temp1 / M));
 
 		W = temp0;
 		b = temp1;
 
+		/*
 		if (step % 10 == 0)
-		{
 			printf("step[%d], cost:%lf, W:[%lf], b:[%lf]\n", step, cost, W, b);
-		}
+			*/
+
+		printf("step[%d], cost:%lf, W:[%lf], b:[%lf]\n", step, cost, W, b);
 
 	}
 }
